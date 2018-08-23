c71bdd223623567ed0d982eff61a64b0027a8259
derrick b <dbozich1@gmail.com>
2018-08-23 23:42:10 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -22,7 +22,7 @@ class Song < ActiveRecord::Base
   def note_contents=(notes)
     binding.pry
     notes.each do |note|
-      
+
       note = Note.create(:content => note)
       self.notes << note
     end
